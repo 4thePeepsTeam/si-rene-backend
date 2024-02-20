d29c03f2e74cb7a3a100f0b675c5f5aaf1494fd6
Almaas Izdihar <almaasizdihar@gmail.com>
2024-02-20 09:09:17 +0700

    refactor: refactor get nearby services

--- src/utils/getNearbyPublicServices.js
+++ src/utils/getNearbyPublicServices.js
@@ -3,18 +3,8 @@ const { collection, getDocs, query, where } = require("firebase/firestore");
 
 async function getNearbyPublicServices({ currentPosition, type }) {
   try {
-    let publicServices = [];
-
-    // read from db
-    const serviceRef = collection(db, "officer");
-    const q = query(
-      serviceRef,
-      where("role", "==", type),
-      where("isOnCall", "==", false),
-      where("isOnDuty", "==", false)
-    );
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => publicServices.push(doc.data()));
+    // get public services
+    let publicServices = await getPublicServices({ type: type });
 
     // get distance all services
     const publicServicesDistance = await Promise.all(
@@ -41,55 +31,82 @@ async function getNearbyPublicServices({ currentPosition, type }) {
       .slice(0, 3);
 
     return publicServices;
-  } catch (err) {
-    throw err;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getPublicServices({ type }) {
+  try {
+    const publicServices = [];
+
+    // read from db
+    const serviceRef = collection(db, "officer");
+    const q = query(
+      serviceRef,
+      where("role", "==", type),
+      where("isOnCall", "==", false),
+      where("isOnDuty", "==", false)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => publicServices.push(doc.data()));
+
+    return publicServices;
+  } catch (error) {
+    throw error;
   }
 }
 
 async function getDistance({ origin, destination }) {
-  const response = await fetch(
-    `https://routes.googleapis.com/directions/v2:computeRoutes`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Goog-Api-Key": process.env.GOOGLE_MAPS_API_KEY,
-        "X-Goog-FieldMask":
-          "routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline",
-      },
-      body: JSON.stringify({
-        origin: {
-          location: {
-            latLng: {
-              latitude: origin.latitude,
-              longitude: origin.longitude,
+  try {
+    const response = await fetch(
+      `https://routes.googleapis.com/directions/v2:computeRoutes`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Goog-Api-Key": process.env.GOOGLE_MAPS_API_KEY,
+          "X-Goog-FieldMask":
+            "routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline",
+        },
+        body: JSON.stringify({
+          origin: {
+            location: {
+              latLng: {
+                latitude: origin.latitude,
+                longitude: origin.longitude,
+              },
             },
           },
-        },
-        destination: {
-          location: {
-            latLng: {
-              latitude: destination.latitude,
-              longitude: destination.longitude,
+          destination: {
+            location: {
+              latLng: {
+                latitude: destination.latitude,
+                longitude: destination.longitude,
+              },
             },
           },
-        },
-        travelMode: "DRIVE",
-        routingPreference: "TRAFFIC_AWARE",
-        departureTime: new Date().toISOString(),
-        computeAlternativeRoutes: false,
-        routeModifiers: {
-          avoidTolls: false,
-          avoidHighways: false,
-          avoidFerries: false,
-        },
-        languageCode: "en-US",
-        units: "IMPERIAL",
-      }),
-    }
-  );
-  const data = await response.json();
-  return data;
+          travelMode: "DRIVE",
+          routingPreference: "TRAFFIC_AWARE",
+          departureTime: new Date(
+            new Date().getTime() + 5 * 60 * 1000
+          ).toISOString(),
+          computeAlternativeRoutes: false,
+          routeModifiers: {
+            avoidTolls: false,
+            avoidHighways: false,
+            avoidFerries: false,
+          },
+          languageCode: "en-US",
+          units: "IMPERIAL",
+        }),
+      }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = getNearbyPublicServices;
